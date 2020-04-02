93d712db8c148a988c211a1524ada4cfe30a496c
Yash <yashrastogi@gmail.com>
2020-04-02 23:18:20 +0530

    Try try-catch

--- main.dart
+++ main.dart
@@ -30,14 +30,12 @@ void printQuery(Object o, io.File logFile) {
 }
 
 Future<List<Uri>> getGoogleImages(String query, {int count = 5}) async {
-  var url = Uri.parse(
-      "https://www.google.com/search?tbm=isch&q=${Uri.encodeFull(query)}");
+  var url = Uri.parse("https://www.google.com/search?tbm=isch&q=${Uri.encodeFull(query)}");
   var document = (await Dio().getUri(url)).data;
   var soup = Beautifulsoup(document);
   List<Uri> res = List();
   soup.find_all('img').forEach((elem) {
-    if (elem.attributes['src'] != null) if (elem.attributes['src']
-        .contains("gstatic.com")) {
+    if (elem.attributes['src'] != null) if (elem.attributes['src'].contains("gstatic.com")) {
       if (count == 0) return;
       count--;
       res.add(Uri.parse(elem.attributes['src']));
@@ -48,47 +46,46 @@ Future<List<Uri>> getGoogleImages(String query, {int count = 5}) async {
 
 void main() {
   var logFile = new io.File('usage.log');
-  var teledart = TeleDart(
-      Telegram(io.Platform.environment['TELEGRAM_API_TOKEN']), Event());
+  var teledart = TeleDart(Telegram(io.Platform.environment['TELEGRAM_API_TOKEN']), Event());
 
   teledart.start().then((me) => print('${me.username} is initialised.'));
 
-  teledart.onCommand('greet').listen(((message) => teledart.replyMessage(
-      message,
+  teledart.onCommand('greet').listen(((message) => teledart.replyMessage(message,
       'Hello, Sir ${message.from.first_name}${message.from.last_name == null ? "" : " " + message.from.last_name}!')));
 
   teledart.onCommand('test').listen((message) {
     printQuery(message, logFile);
   });
 
-  teledart
-      .onMessage(keyword: 'dart')
-      .where((message) => message.text.contains('telegram'))
-      .listen((message) => teledart.replyPhoto(message,
-          'https://raw.githubusercontent.com/DinoLeung/TeleDart/master/example/dash_paper_plane.png',
+  teledart.onMessage(keyword: 'dart').where((message) => message.text.contains('telegram')).listen((message) => teledart
+      .replyPhoto(message, 'https://raw.githubusercontent.com/DinoLeung/TeleDart/master/example/dash_paper_plane.png',
           caption: 'This is how the Dart Bird and Telegram, met'));
 
-  teledart
-      .onInlineQuery()
-      .listen((inlineQuery) => {
-            printQuery(inlineQuery, logFile),
-            getGoogleImages(inlineQuery.query, count: 12).then((urls) {
-              var results = new List<InlineQueryResult>();
-              int count = 0;
-              for (var url in urls) {
-                count++;
-                results.add(
-                  InlineQueryResultPhoto()
-                    ..caption = count.toString()
-                    ..thumb_url = url.toString()
-                    ..photo_url = url.toString()
-                    ..id = inlineQuery.query + count.toString(),
-                );
-              }
-              teledart.answerInlineQuery(inlineQuery, results);
-            }),
-          })
-      .onError((handleError) => print(handleError));
+  try {
+    teledart
+        .onInlineQuery()
+        .listen((inlineQuery) => {
+              printQuery(inlineQuery, logFile),
+              getGoogleImages(inlineQuery.query, count: 12).then((urls) {
+                var results = new List<InlineQueryResult>();
+                int count = 0;
+                for (var url in urls) {
+                  count++;
+                  results.add(
+                    InlineQueryResultPhoto()
+                      ..caption = count.toString()
+                      ..thumb_url = url.toString()
+                      ..photo_url = url.toString()
+                      ..id = inlineQuery.query + count.toString(),
+                  );
+                }
+                teledart.answerInlineQuery(inlineQuery, results);
+              }),
+            })
+        .onError((handleError) => print("OnError: ${handleError}"));
+  } catch (e) {
+    print("Catch: ${e}");
+  }
 
   logFile.watch().listen((onData) {
     logFile.stat().then((onValue) {
